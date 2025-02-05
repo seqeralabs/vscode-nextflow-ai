606f0e8255edb2390d0b7935b08d68413aa6ee2c
Jake Broughton <jake.broughton@seqera.io>
2025-02-05 18:14:45 +0100

    Add telemetry

--- src/extension.ts
+++ src/extension.ts
@@ -5,13 +5,16 @@ import {
 } from "./activateLanguageServer";
 import { activateChatbot } from "./activateChatbot";
 import { activateReadme } from "./activateReadme";
+import { activateTelemetry, deactivateTelemetry } from "./activateTelemetry";
 
 export function activate(context: vscode.ExtensionContext) {
   activateReadme(context);
+  activateTelemetry(context);
   activateChatbot(context);
   activateLanguageServer(context);
 }
 
 export function deactivate(): Thenable<void> | undefined {
+  deactivateTelemetry();
   return stopLanguageServer();
 }
