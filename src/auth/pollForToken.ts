b8674e1e73f5c65740bfcab66602e13f9d7539e7
Jake Broughton <jake.broughton@seqera.io>
2025-02-07 16:11:44 +0100

    Refactor, fix polling

--- src/auth/pollForToken.ts
+++ src/auth/pollForToken.ts
@@ -8,51 +8,68 @@ type TokenResponse = {
   token_type: string;
   expires_in: number;
   scope?: string;
+  error?: string;
 };
 
-async function pollForToken(config: Auth0Config): Promise<TokenResponse> {
+async function fetchToken(config: Auth0Config): Promise<Response> {
   const tokenEndpoint = `https://${auth0Domain}/oauth/token`;
+  return await fetch(tokenEndpoint, {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams({
+      grant_type: "urn:ietf:params:oauth:grant-type:device_code",
+      device_code: config.device_code,
+      client_id,
+    }),
+  });
+}
+
+async function pollForToken(
+  config: Auth0Config
+): Promise<TokenResponse | null> {
   const startTime = Date.now();
   const interval = config.interval * 1000;
   const expiresIn = config.expires_in * 1000;
+
+  console.log("🟢 Seqera: Polling for token");
 
   while (true) {
     // Sleep a moment
     await new Promise((resolve) => setTimeout(resolve, interval));
 
     // Attempt to fetch the token
-    const response = await fetch(tokenEndpoint, {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: new URLSearchParams({
-        grant_type: "urn:ietf:params:oauth:grant-type:device_code",
-        device_code: config.device_code,
-        client_id,
-      }),
-    });
+    const response = await fetchToken(config);
+    if (!response) continue;
 
-    // Return token if successful
-    if (response.ok) {
-      const responseBody = await response.json();
-      return responseBody as TokenResponse;
-    }
+    const responseBody = (await response.json()) as TokenResponse;
+    const errorCode = responseBody.error;
 
-    const timeElapsed = Date.now() - startTime;
-    const errorBody: any = await response.json();
-    const errorCode = errorBody.error;
+    // Return token on success
+    if (!errorCode) return responseBody;
+
     const isPending = errorCode === "authorization_pending";
-    const hasError = !isPending;
+    const timeElapsed = Date.now() - startTime;
 
-    // Exit if there's an error
-    if (hasError) {
-      throw new Error(`Device flow error: ${errorCode}`);
-    }
+    // Repeat if pending
+    if (isPending) continue;
 
-    // Exit if the timeout has expired
+    // Exit if time exceeded
     if (timeElapsed > expiresIn) {
       throw new Error("Device flow timeout expired.");
     }
+
+    // Otherwise exit
+    throw new Error(`Device flow error: ${errorCode}`);
   }
 }
 
-export default pollForToken;
+export default async (config: Auth0Config): Promise<TokenResponse | null> => {
+  try {
+    const token = await pollForToken(config);
+    return token;
+  } catch (error: any) {
+    console.log("🔴 Seqera:", error.message);
+    console.error(error);
+    return null;
+  }
+};
