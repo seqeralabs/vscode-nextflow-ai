1f2112d7dee0d05b00f764debcdee5dc0fb4945b
Jake Broughton <jake.broughton@seqera.io>
2025-02-20 16:01:29 +0100

    Fix extension name

--- src/activateAuth.ts
+++ src/activateAuth.ts
@@ -14,7 +14,7 @@ export async function activateAuth(context: vscode.ExtensionContext) {
     }
   });
 
-  const loginCmd = vscode.commands.registerCommand("seqera-ai.login", () => {
+  const loginCmd = vscode.commands.registerCommand("nextflow.login", () => {
     startDeviceFlow(context);
   });
 
