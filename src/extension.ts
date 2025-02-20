nextflow-io/vscode-language-nextflow

Test event, track basic commands

src/extension.ts
@@ -4,9 +4,9 @@ import activateChatbot from "./activateChatbot";
 import { activateTelemetry, deactivateTelemetry } from "./activateTelemetry";
 
 export function activate(context: vscode.ExtensionContext) {
-  activateLanguage(context);
-  activateChatbot(context);
-  activateTelemetry(context);
+  const trackEvent = activateTelemetry(context);
+  activateLanguage(context, trackEvent);
+  activateChatbot(context, trackEvent);
 }
 
 export function deactivate(): Thenable<void> | undefined {

seqeralabs/vscode-nextflow-ai

src/extension.ts
@@ -8,10 +8,10 @@ import { activateReadme } from "./activateReadme";
 import { activateTelemetry, deactivateTelemetry } from "./activateTelemetry";
 
 export function activate(context: vscode.ExtensionContext) {
+  const trackEvent = activateTelemetry(context);
   activateReadme(context);
-  activateTelemetry(context);
-  activateChatbot(context);
-  activateLanguageServer(context);
+  activateLanguageServer(context, trackEvent);
+  activateChatbot(context, trackEvent);
 }
 
 export function deactivate(): Thenable<void> | undefined {